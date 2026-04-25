280484938d204456cacf2eef749feacc7486c72d
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-04-25 21:24:19 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,32 @@
+
+console.log(2>1)
+console.log(2<1)
+console.log(2>=2)
+console.log(2<=1)
+console.log(2==2)
+console.log(2== "2")
+console.log(2=== "2")
+console.log(0 == false)
+console.log(0 === false)
+
+
+
+
+
+
+console.log(undefined == null)
+console.log(undefined === null)
+console.log(undefined > null)
+console.log(undefined < null)
+console.log("2" > null)
+console.log("2" < null)
+console.log("2" == null)
+
+
+
+
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null == 0)
+console.log(null >= 0)
