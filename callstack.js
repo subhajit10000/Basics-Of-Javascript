973d063197a881b658a63143200b630be5570f41
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-04-27 18:53:46 +0530

    Add files via upload

---
+++ callstack.js
@@ -0,0 +1,36 @@
+
+function one(){
+
+    console.log("one")
+
+    two()
+
+    console.log("new one")  
+
+}
+
+
+
+function two(){
+
+    console.log("two")
+
+    three()
+
+    console.log("two")
+
+}
+
+
+
+function three(){
+
+    console.log("three")
+
+}
+
+
+
+
+
+one()
