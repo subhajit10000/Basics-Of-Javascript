f546ffa7f4fdd4898a481d4ef07ceab6ae9d029b
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-04-30 19:43:32 +0530

    Add files via upload

---
+++ while_dowhile.js
@@ -0,0 +1,37 @@
+//while loop
+
+
+let index = 0
+while (index < 10) {
+    console.log(index)
+    index++;
+}
+
+
+
+
+
+let myArr = [1, 2, 3, 4]
+let arr = 0
+
+
+while(arr < myArr.length){
+    console.log(myArr[arr])
+    arr++;
+}
+
+
+
+
+
+
+//do while loop
+
+
+let score = 0
+
+
+do {
+    console.log(`score is ${score}`)
+    score++;
+} while (score > 1);
