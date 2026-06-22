964175be72682e17a23576b40f88c23d618cfebf
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-06-22 09:32:43 +0530

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,1 +1,15 @@
+// Basic Promise Example
 
+const myPromise = new Promise((resolve, reject) => {
+    let success = true;
+
+    if (success) {
+        resolve("Promise resolved successfully!");
+    } else {
+        reject("Promise rejected!");
+    }
+});
+
+myPromise
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
